d2646f48485233a57abe95a9e7072df2325357b9
baiyun <yun.bai1@newtouch.com>
2022-02-11 18:02:27 +0800

    代码优化、增加单例模式实现代码

--- DesignPattern_Strategy/src/com/test/service/demo1/test/CountPriceContextTest.java
+++ DesignPattern_Strategy/src/com/test/service/demo1/test/CountPriceContextTest.java
@@ -2,14 +2,15 @@ package com.test.service.demo1.test;
 
 
 import com.test.service.demo1.context.CountPriceContext;
+import com.test.service.demo1.model.PayEnum;
 
 public class CountPriceContextTest {
 
 	public static void main(String[] args) {
 		CountPriceContext context = new CountPriceContext();
-		Double countPriceJD = context.countPrice(1, 10000.0);
-		Double countPriceWX = context.countPrice(2, 10000.0);
-		Double countPriceZFB = context.countPrice(3, 10000.0);
+		Double countPriceJD = context.countPrice(PayEnum.JD.getType(), 10000.0);
+		Double countPriceWX = context.countPrice(PayEnum.WX.getType(), 10000.0);
+		Double countPriceZFB = context.countPrice(PayEnum.ZFB.getType(), 10000.0);
 		System.out.println("京东折扣价：" + countPriceJD + "\r微信折扣价：" + countPriceWX + "\r支付宝折扣价：" + countPriceZFB);
 		
 	}
